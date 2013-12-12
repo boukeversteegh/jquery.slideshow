4e276baaf45f59265de86571d7a45ffa0f051282
Bouke Versteegh <info@boukeversteegh.nl>
2013-12-12 12:57:44 +0100

    Configurable selectors (classnames) for items

--- jquery.slideshow.js
+++ jquery.slideshow.js
@@ -12,7 +12,15 @@
     /* default options */
     var defaults = {
         duration:   200,
-        direction:  'horizontal'
+        direction:  'horizontal',
+        selectors: {
+            slides:     '> .slides',
+            slide:      '> .slide',
+            controls:   '> .slide-controls',
+            button:     '.btn',
+            prev:       '.btn-prev',
+            next:       '.btn-next'
+        }
     };
 
     /* plugin methods */
@@ -26,72 +34,80 @@
                 options = {};
             }
             $(this).each(function() {
-                var $slideshow = $(this);
-
-                var settings = $.extend({}, defaults);
-                $.extend(settings, options);
+                var settings = $.extend(true, {}, defaults);
+                $.extend(true, settings, options);
                 
+                var $slideshow  = $(this);
                 $slideshow.data('settings', settings);
+                var selectors = settings.selectors;
 
-                var fontsize = $slideshow.find('> .slides > .slide').css('font-size');
-                fontsize = fontsize ? fontsize : 'inherit';
                 
-                var position = $slideshow.css('position');
+                var $slides     = $slideshow.find(selectors.slides);
+                var $slide      = $slides.find(selectors.slide);
+                var $controls   = $slideshow.find(selectors.controls);
+                var $buttons    = $slideshow.find([selectors.button, selectors.prev, selectors.next].join(','));
+                var position    = $slideshow.css('position');
 
                 // Slideshow css-position must be relative or absolute
-                if( $.inArray(position, ['relative', 'absolute']) < 0 ) {
+                if( $.inArray(position, ['relative', 'absolute', 'fixed']) < 0 ) {
                     position = 'relative';
                 }
+
                 $slideshow.addClass('slideshow').css({
                     'position':    position
-                }).find('> .slides').css({
-                    'overflow':    'hidden'
-                }).find('> .slide').css({
-                    
                 });
 
-                $slideshow.find('> .slide-controls').find('.btn, .btn-next, .btn-prev').css({
+                $slides.css({
+                    'overflow':    'hidden'
+                });
+
+                $buttons.css({
                     'cursor':     'pointer'
                 });
-                
+
+                var fontsize = $slide.css('font-size');
+                fontsize = fontsize ? fontsize : 'inherit';
+
                 if( settings.direction == 'horizontal' ) {
-                    $slideshow.find('> .slides').css({
+                    $slides.css({
                         'white-space': 'nowrap',
                         'font-size':   0
-                    }).find('> .slide').css({
-                        'display':     'inline-block',
-                        'font-size':   fontsize,
-                        'width':       '100%',
-                        'white-space': 'normal',
-                        'vertical-align': 'top'
+                    });
+                    $slide.css({
+                        'display':          'inline-block',
+                        'font-size':        fontsize,
+                        'width':            '100%',
+                        'white-space':      'normal',
+                        'vertical-align':   'top'
                     })
                 } else {
                     $slideshow.css({
                         'overflow':     'hidden'
-                    }).find('> .slides').css({
+                    });
+                    $slides.css({
                         'position':     'relative',
                         'height':       '100%'
-                    }).find('> .slide').css({
+                    });
+                    $slide.css({
                         'display':      'block',
                         'height':       '100%'
                     });
                 }
 
                 // Bind slide-controls buttons directly under current slideshow
-                $('> .slide-controls .btn', $slideshow).click(function() {
-                    var index = $(this).closest('.slide-controls').find('> .btn').index(this);
-                    var $slideshow = $(this).closest('.slideshow');
+                $(selectors.controls, $slideshow).find(selectors.button).click(function() {
+                    var controls_selector = selectors.controls.match('(.*[>\\s])?(.+)').pop();
+                    var index = $(this).closest(controls_selector).find('> .btn').index(this);
                     methods.showslide.apply($slideshow, [index]);
                 });
 
-                $('> .slide-controls .btn-prev, > .slide-controls .btn-next', $slideshow).click(function() {
-                    var $slideshow = $(this).closest('.slideshow');
+                $(selectors.controls, $slideshow).find([selectors.prev, selectors.next].join(',')).click(function() {
                     var index = $slideshow.data('index');
 
                     if( index === undefined ) {
                         index = 0;
                     }
-                    var delta = $(this).is('.btn-prev') ? -1 : 1;
+                    var delta = $(this).is(settings.selectors.prev) ? -1 : 1;
                     methods.showslide.apply($slideshow, [index + delta]);
                 });
             });
@@ -104,11 +120,11 @@
          * @param {number} animate Slide animation duration. False or 0 for no animation.
          */
         showslide: function(index, animate) {
-			index = ( typeof index != 'undefined' ) ? index : 0;
+            index = ( typeof index != 'undefined' ) ? index : 0;
             $(this).each(function() {
                 var $slideshow = $(this);
-                var $slides = $slideshow.children('.slides');
                 var settings = $slideshow.data('settings');
+                var $slides = $slideshow.find(settings.selectors.slides);
 
                 animate = (animate === undefined ? true: animate);
                 if( index < 0 ) {
@@ -120,8 +136,8 @@
                 
                 var duration = (animate ? settings.duration : 0);
                 
-                var $slide  = $slides.find('> .slide').eq(index);
-                var $btn    = $slideshow.find('> .slide-controls > .btn').eq(index);
+                var $slide  = $slides.find(settings.selectors.slide).eq(index);
+                var $btn    = $slideshow.find(settings.selectors.controls).find(settings.selectors.button).eq(index);
                 
                 var offset = null;
                 
